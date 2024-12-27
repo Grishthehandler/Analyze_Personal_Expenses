fe0f03dcda5c021bf6f83eb8f1fba682575f4106
Grishthehandler <gupta.grish03@gmail.com>
2024-12-27 09:18:15 +0530

    this is the second setup

---
+++ src/app.py
@@ -0,0 +1,18 @@
+# loading module 
+
+import pandas as pd
+
+import numpy as np
+
+from faker import Faker
+
+import random
+
+from mysql.connector import connection
+
+import streamlit as st
+
+from datetime import datetime
+
+
+
